9efa420070d33cffcba24fc6c92a361480d08919
danStich <daniel.stich@oneonta.edu>
2018-04-17 01:45:48 -0400

    data and models to go with them

--- elliptioPhenology.R
+++ elliptioPhenology.R
@@ -1,11 +1,48 @@
 # Front-end needs -----
 # . Package load -----
-packages <- c("dplyr", "lubridate", "geosphere", "lme4", "plyr")
-lapply(packages, library, character.only = TRUE)
+# Make a list of packages to load
+  packages <- c("dplyr",
+                "lubridate",
+                "geosphere", 
+                "lme4",
+                "plyr",
+                "boot",
+                "waterData",
+                "piecewiseSEM",
+                "AICcmodavg"
+                )
+# Load them
+  lapply(packages, library, character.only = TRUE)
 
 # Data manipulation -----
+# . Climate data -----
+# Discharge data
+  # Import discharge data from the USGS gage on
+  # the Susquehanna River at Unadilla, NY
+    usgs = waterData::importDVs("01500500",
+                                       sdate="2016-01-01",
+                                       edate="2016-12-31"
+                                       )
+  # Convert to cubic meters per second
+    usgs$cms = usgs$val * 0.028316847
+  # Change date columnn name to match the others
+    names(usgs)[3] = 'date'
+    
+# NOAA climate data
+  # Read in the data file
+    noaa = read.csv('noaaClimateData.csv')
+  # Compute daily means from max and min
+    noaa$tmean = apply(noaa[,c(8:9)],1,mean)
+  # Change date to correct format
+    noaa$date = as.Date(as.character(noaa$DATE), format="%m/%d/%Y")
+  # Calculate degree day
+    ddprep = noaa$tmean
+    ddprep[ddprep<0] = 0
+    noaa$dd = cumsum(ddprep)
+  
+# . Elliptio data
 # Read in the data
-  ellip = read.csv('elliptioData.csv', stringsAsFactors = FALSE)
+  ellip = read.csv('elliptioData_aug.csv', stringsAsFactors = FALSE)
   head(ellip)
   str(ellip)
     
@@ -21,44 +58,122 @@ lapply(packages, library, character.only = TRUE)
 # Add column for day of year
   ellip$doy = lubridate::yday(ellip$date)
 
-# Square the doy
-  ellip$doy2 = ellip$doy^2
-    
-# Standardize doy and doy2
+# . Data merge and covariates -----
+# Merge the mussel data with the flow data
+  climate = merge(usgs, noaa)      
+  ellip = merge(ellip, climate)   
+
+# Calculate photoperiod
+  ellip$photo = daylength(lat=ellip$LATITUDE, doy = ellip$doy)
+ 
+# Standardize day of year
   ellip$sdoy = as.vector(scale(ellip$doy))
-  ellip$sdoy2 = as.vector(scale(ellip$doy2))
+         
+# Standardize photoperiod
+  ellip$sphoto = as.vector(scale(ellip$photo))
   
+# Standardize temperature
+  ellip$stmean = as.vector(scale(ellip$tmean))
+
+# Standardize dd
+  ellip$sdd = as.vector(scale(ellip$dd))
+
+# Standardize discharge
+  ellip$scms = as.vector(scale(ellip$cms))
+
 # Remove missing observations for gametes
-  ellip = ellip[!is.na(ellip$gametes), ]
+  # ellip = ellip[!is.na(ellip$gametes), ]
   
 # Make a new data frame with just females
-  #ellip = ellip[ellip$sex=='F', ]
-  
+  # ellip = ellip[ellip$sex=='F', ]
+    
 # Data analysis -----
-# Fit a model to describe changes in Elliptio gamete
-# expression as a function of date with a random
-# effect on the intercept for individual
-  pmod = lme4::glmer(formula = gametes~sdoy+I(sdoy^2)+(1|tag),
-                     data=ellip, family = 'binomial'
-                     )
-  summary(pmod)
+# Fit a list of models to describe changes in Elliptio gamete
+# expression as a function of environmental factors with 
+# a random effect on the intercept for individual
+
+# Define an empty list
+  pmods = list()
+  
+# Add each of the models to the list  
+  pmods[[1]] = lme4::glmer(formula = gametes~stmean+I(stmean^2)+(1|tag),
+                           data=ellip,
+                           family = 'binomial'
+                           )
+  pmods[[2]] = lme4::glmer(formula = gametes~sdd+I(sdd^2)+(1|tag),
+                           data=ellip,
+                           family = 'binomial'
+                           )
+  pmods[[3]] = lme4::glmer(formula = gametes~sphoto+I(sphoto^2)+(1|tag),
+                           data=ellip,
+                           family = 'binomial'
+                           )     
+  pmods[[4]] = lme4::glmer(formula = gametes~scms+I(scms^2)+(1|tag),
+                           data=ellip,
+                           family = 'binomial'
+                           )   
+  pmods[[5]] = lme4::glmer(formula = gametes~stmean+I(stmean^2)+
+                           scms+I(scms^2)+(1|tag),
+                           data=ellip,
+                           family = 'binomial'
+                           )
+  pmods[[6]] = lme4::glmer(formula = gametes~sdd+I(sdd^2)+
+                           scms+I(scms^2)+(1|tag),
+                           data=ellip,
+                           family = 'binomial'
+                           )
+  pmods[[7]] = lme4::glmer(formula = gametes~sphoto+I(sphoto^2)+
+                           scms+I(scms^2)+(1|tag),
+                           data=ellip,
+                           family = 'binomial'
+                           )  
+
+# Name the models in the list  
+  # Loop through the list and name each
+  # model using the formula from the fit
+  for(i in 1:length(pmods)){
+    names(pmods)[i] = as.character(summary(pmods[[i]])$call[2])
+  }
+  # Use gsub to eliminate response var from names
+  names(pmods) = gsub(pattern = 'gametes ~ ',
+                      replacement = '',
+                      x = names(pmods)
+                      )
+  # Use gsub to eliminate random effect from names
+  names(pmods) = gsub(pattern = ' + (1 | tag)',
+                      replacement = '',
+                      x = names(pmods)
+                      )  
+  
+# Make a model selection table for the models
+  tab = aictab(pmods, names(pmods))
+  tab
+  
+# Extract R-squared values based on:
+# Nakagawa, S., and H. Schielzeth. 2013. A general
+#      and simple method for obtaining R2 from 
+#      generalized linear mixed-effects models.
+#      Methods in Ecology and Evolution 4(2):133-142.
+  rsquared(pmods)
+  
   
 # Model predictions -----
 
 # Make a sequence of new doy
-  ndoy = seq(-3, 3, by=.01)
+  ndd = seq(-3, 3, by=.01)
   
 # Make a df that contains lengths
 # and a label for population
   newd = data.frame(
-    tag = rep(ntag, length(ndoy)),
-    sdoy = rep(ndoy, length(unique(ntag)))    
+    sdd = ndd    
   )
 
 # Simulate predictions from the relationship
 # stored in the model fit using our new data
-# Extract coefficients to an object with shorter name
-  res = summary(pmod)$coefficients
+  
+# Extract coefficients from best model using
+# row names from the model selection table
+  res = summary(pmods[[as.numeric(row.names(tab))[1]]])$coefficients
   
 # Get means of regression coeffs
   int = res[1,1]
@@ -74,33 +189,39 @@ lapply(packages, library, character.only = TRUE)
   ub2 = res[3,1]+1.96*res[3,2]
   
 # Make predictions  
-  pred = invlogit(int+b1*newd$sdoy+b2*newd$sdoy^2)
-  lpred = invlogit(lint+lb1*newd$sdoy+lb2*newd$sdoy^2)
-  upred = invlogit(uint+ub1*newd$sdoy+ub2*newd$sdoy^2)  
+  pred = boot::inv.logit(int+b1*newd$sdd+b2*newd$sdd^2)
+  lpred = boot::inv.logit(lint+lb1*newd$sdd+lb2*newd$sdd^2)
+  upred = boot::inv.logit(uint+ub1*newd$sdd+ub2*newd$sdd^2)  
   
 # Plot raw data and predictions
   # Graphical parameters
   par(mar=c(5,5,1,1))
 
   # Raw data and plotting region set up
-  plot(x=ellip$sdoy,
+  plot(x=ellip$sdd,
        y=ellip$gametes,
        pch=21,bg=rgb(.1,.1,.1,.05),
        cex=2, cex.lab=1.10,
-       xlab='Day of year', ylab='p ( gametes | day )',
+       xlab='Accumulated thermal units (ATU)',
+       ylab='p ( gametes | ATU )',
        xlim=c(-2, 2), ylim=c(0,1), xaxt='n', yaxt='n')
   
+  # Add a shaded region for the CI
+  xx = c((newd$sdd), rev(newd$sdd))
+  yy = c(c(lpred), rev(c(upred)))
+  polygon(xx, yy, col='gray87', border='gray87')
+  
   # Lines for mean and 95% CI of predictions
-  lines(newd$sdoy, lpred, col='red', lwd=2, lty=2)
-  lines(newd$sdoy, upred, col='red', lwd=2, lty=2)
-  lines(newd$sdoy, pred, col='blue', lwd=2, lty=1)
+  lines(newd$sdd, lpred, col='red', lwd=2, lty=2)
+  lines(newd$sdd, upred, col='red', lwd=2, lty=2)
+  lines(newd$sdd, pred, col='blue', lwd=2, lty=1)
   
   # Add white points to cover up old points and lines
-  points(x=ellip$sdoy, y=ellip$gametes, pch=21, bg='white', cex=2)
+  points(x=ellip$sdd, y=ellip$gametes, pch=21, bg='white', cex=2)
 
   # Add transparent points to increase opacity for
   # larger sample sizes
-  points(x=ellip$sdoy,
+  points(x=ellip$sdd,
        y=ellip$gametes,
        pch=21,bg=rgb(0.1,0.1,0.1,0.05),
        cex=2)
@@ -112,12 +233,8 @@ lapply(packages, library, character.only = TRUE)
          #Get calendar date from ordinal date using
          # back-transformed values of doy from a sequence
          # of new standardized values
-           chron::chron(
-             round(
-               seq(-2, 2, 0.5)*sd(ellip$doy)+mean(ellip$doy)
-               ),
-             origin.=c(month=1, day=1, year=2016),
-             out.format="day mon"
+           round(
+             seq(-2, 2, 0.5)*sd(ellip$dd)+mean(ellip$dd)
              )
        )
   
@@ -128,16 +245,20 @@ lapply(packages, library, character.only = TRUE)
     # Get proportion of brooders in each 
     # sample
     ellip$counts = 1
-    pr = plyr::ddply(ellip[ellip$sex=='F',],
-                     c('date', 'doy'),
+    pr = plyr::ddply(ellip,#[ellip$sex=='F',],
+                     'dd',
                      summarize,
-                     pBrooder = sum(brooding)/sum(counts)
+                     pBrooder = sum(brooding)/sum(counts),
+                     pgamete = sum(gametes)/sum(counts)
                      )
-    points(x=(pr[,2]-mean(ellip$doy))/sd(ellip$doy),
+    points(x=(pr[,1]-mean(ellip$dd))/sd(ellip$dd),
            y=pr[,3],
            pch=21,
            bg='white'
            )
+    
+  # Add the box to the outside
+    box()
     
   
   
